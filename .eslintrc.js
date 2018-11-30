3b936bf176759b65e585ae646790350875f7076c
Gennady Dogaev <gendelflg@gmail.com>
2018-12-01 01:43:24 +0200

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,37 +2,42 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'ember'
-  ],
+  plugins: ['ember', 'prettier'],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
   ],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
+    'no-inline-comments': 'off',
+    'prettier/prettier': 'warn',
   },
   overrides: [
     // node files
     {
       files: [
-        'testem.js',
+        '.eslintrc.js',
+        '.prettierrc.js',
+        '.template-lintrc.js',
         'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
-      }
-    }
-  ]
+        node: true,
+      },
+    },
+  ],
 };
