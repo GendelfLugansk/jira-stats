802ecc3a248ab1587a69efb060989722c6bd4788
Gennady Dogaev <gendelflg@gmail.com>
2018-12-10 03:12:29 +0200

    Add histogram of time spent

---
+++ app/components/reports/time-spent-histogram.js
@@ -0,0 +1,61 @@
+import Component from '@ember/component';
+import ChartMixin from './chart-mixin';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import math from 'npm:mathjs';
+import { observer } from '@ember/object';
+import { next } from '@ember/runloop';
+
+export default Component.extend(ChartMixin, {
+  backend: service(),
+  chartColors: service(),
+
+  fetch: task(function*() {
+    const traces = yield this.backend.chartTimeSpentHistogram({
+      filters: this.filters,
+    });
+
+    if (traces.length === 0) {
+      this.set('plotlyData', undefined);
+      return;
+    }
+
+    traces.forEach(trace => {
+      trace.marker = {
+        color: this.chartColors.getColor('assignee', trace.name),
+      };
+    });
+
+    traces.sort((a, b) =>
+      math.number(
+        math.subtract(
+          math.median(b.x.map(x => math.bignumber(x))),
+          math.median(a.x.map(x => math.bignumber(x)))
+        )
+      )
+    );
+
+    this.set('plotlyData', traces);
+    this.set('plotlyLayout', {
+      height: 500,
+      margin: {
+        r: 60,
+        t: 50,
+        b: 50,
+        l: 40,
+      },
+      barmode: 'overlay',
+      hovermode: 'closest',
+      colorway: this.get('chartColors.colors'),
+      bargap: 0.05,
+      bargroupgap: 0.2,
+      xaxis: { title: 'Time Spent, hours' },
+      yaxis: { title: 'Percent of Issues' },
+      showlegend: true,
+    });
+  }).drop(),
+
+  _filtersObserver: observer('filters', function() {
+    next(this.fetch, 'perform');
+  }),
+});
--- app/services/backend/charts/common.js
+++ app/services/backend/charts/common.js
@@ -1,4 +1,5 @@
 import Mixin from '@ember/object/mixin';
+import math from 'npm:mathjs';
 import $ from 'jquery';
 
 const { extend } = $;
@@ -34,6 +35,57 @@ export default Mixin.create({
             size: 0.1,
           },
         });
+      }
+    }
+
+    traces.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+
+    return traces;
+  },
+
+  async chartTimeSpentHistogram({ filters } = {}) {
+    const { issuesCollection } = await this.ensureCollections();
+    const groupedIssues = issuesCollection.find(
+      extend({}, this.filtersToQuery(filters), {
+        time_spent: {
+          $exists: true,
+        },
+      }),
+      {
+        $groupBy: {
+          assignee: 1,
+        },
+      }
+    );
+
+    const traces = [];
+    for (let k in groupedIssues) {
+      if (groupedIssues.hasOwnProperty(k) && Array.isArray(groupedIssues[k])) {
+        let x = groupedIssues[k]
+          .map(issue => issue.time_spent)
+          .filter(t => /^[\d]+(\\.\d+)?$/.test(t))
+          .map(t =>
+            math.number(math.divide(math.bignumber(t), math.bignumber(3600)))
+          );
+        if (x.length > 0) {
+          traces.push({
+            name: k === '' ? 'unassigned' : k,
+            x,
+            autobinx: false,
+            histfunc: 'count',
+            histnorm: 'percent',
+            opacity: 0.6,
+            type: 'histogram',
+            xbins: {
+              start: 0,
+              size: 0.5,
+            },
+          });
+        }
       }
     }
 
